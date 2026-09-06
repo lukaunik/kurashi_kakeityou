360cd5f5b86304a9ba64828a88434d2171d5015a
lukaunik <lukaunik@gmail.com>
2026-09-06 16:07:33 +0900

    Remove hardcoded Firebase API keys and add secure config injection

---
+++ firebase-config.example.js
@@ -0,0 +1,12 @@
+﻿// Firebase初期設定テンプレート
+// ローカルで動かす場合は、このファイルを 'firebase-config.js' にリネームし、
+// ご自身のFirebaseプロジェクトの設定値を入力してください。
+window.FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
